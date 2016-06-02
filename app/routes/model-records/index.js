a37b2dac16f9994d5fd491228e72dda8068e6978
Szabo Bogdan <bogdan.szabo@altom.ro>
2016-06-02 16:55:47 +0300

    Delete records

--- app/routes/model-records/index.js
+++ app/routes/model-records/index.js
@@ -1,1 +1,28 @@
-export { default } from 'ember-admin/routes/model-records/index';
+import Ember from 'ember';
+
+const {
+  get,
+  Route
+} = Ember;
+
+export default Route.extend({
+  model() {
+    return this.admin.store.findAll(this.paramsFor('model-records').name, { reload: true }).then(function(records) {
+      return records.filter(function(item) {
+        return !get(item, 'isNew');
+      });
+    });
+  },
+
+  actions: {
+    destroyRecord(id) {
+      let modelName = this.paramsFor('model-records').name;
+
+      this.admin.store.findRecord(modelName, id).then((item) => {
+        item.destroyRecord().then(() => {
+          this.refresh();
+        });
+      });
+    }
+  }
+});
